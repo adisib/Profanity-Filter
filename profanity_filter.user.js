add0f21833cd20329235467524306e73151b153f
adisib <alexdisibio@gmail.com>
2016-04-29 20:17:22 -0400

    Slightly improve word ordering to match commonality

--- profanity_filter.user.js
+++ profanity_filter.user.js
@@ -25,8 +25,8 @@
     const replaceString = "*bleep*";
 
     // words to be filtered list
-    // This should be ordered by most common first for performance (still largely TODO, but not important)
-    const words = ['fuck','shit','ass','asshole','damn','bullshit','shitty','bitch','shittings','shitters','shitter','shitted','shitfull','shiz','sh!t','piss','pissoff','pissin','nigger','nigga','motherfuckin','motherfuck','mothafuckin','mothafuckaz','mothafucka','mothafuck','jackass','goddamn','fuk','fuckme','fuckin','fcuk','crap','phuq','phukking','phuk','phuck','feg','fatass','fags','faggot','fagot','faggit','fagging','fagget','fag','dumbass','douche','dipshit','bastard','cunt','assholes','buttfuck','bitchin','asswipe','asskisser'];
+    // This should be ordered by most common first for performance (still TODO, but not important)
+    const words = ['fuck','shit','ass','damn','asshole','bullshit','shitty','bitch','piss','sh!t','jackass','goddamn','crap','bastard','assholes','cunt','dumbass','fag','douche','shittings','shitters','shitter','shitted','shitfull','shiz','pissoff','pissin','nigger','nigga','motherfuckin','motherfuck','mothafuckin','mothafuckaz','mothafucka','mothafuck','fuk','fuckme','fuckin','fcuk','phuq','phukking','phuk','phuck','feg','fatass','fags','faggot','fagot','faggit','fagging','fagget','dipshit','buttfuck','bitchin','asswipe','asskisser'];
 
     // filters the words and any versions with optional endings
     // shouldn't run into issues with optional endings; a whitelist would be trivial to implement should it be required
