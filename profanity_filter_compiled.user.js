533c4e41a41d47c28640fefb07b0fcf10cdad96e
Alex Disibio <alexdisibio@gmail.com>
2016-05-07 13:47:41 -0400

    Update Compiled Version

--- profanity_filter_compiled.user.js
+++ profanity_filter_compiled.user.js
@@ -18,7 +18,7 @@
 
 (function(){
 //let st = performance.now();
-let a=new RegExp("\\b(fuck|shit|ass|damn|asshole|bullshit|shitty|bitch|piss|sh!t|jackass|goddamn|crap|bastard|assholes|cunt|dumbass|fag|douche|shittings|shitters|shitter|shitted|shitfull|shiz|pissoff|pissin|nigger|nigga|motherfuckin|motherfuck|mothafuckin|mothafuckaz|mothafucka|mothafuck|fuk|fuckme|fuckin|fcuk|phuq|phukking|phuk|phuck|feg|fatass|fags|faggot|fagot|faggit|fagging|fagget|dipshit|buttfuck|bitchin|asswipe|asskisser)(ing|ed|er)?(es|s)?\\b", "gi"),b=document.evaluate(".//*[not(self::script or self::noscript or self::textarea)]/text()[string-length(normalize-space(.)) > 2]",
+let a=new RegExp("\\b(?:fuck|shit|ass|damn|asshole|bullshit|shitty|bitch|piss|sh!t|jackass|goddamn|crap|bastard|assholes|cunt|dumbass|fag|douche|shitt|shitfull|shiz|pissoff|nigger|nigga|motherfuck|mothafuckaz|mothafucka|mothafuck|fuk|fuckme|fcuk|phuq|phukk|phuk|phuck|feg|fatass|faggot|fagot|faggit|fagg|fagget|dipshit|buttfuck|asswipe|asskisser)(?:in(?:g)?|ed|er)?(?:es|s)?\\b", "gi"),b=document.evaluate(".//*[not(self::script or self::noscript or self::code or self::textarea)]/text()[string-length(normalize-space(.)) > 2]",
 document.body,null,XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE,null);-1!==document.title.search(a)&&(document.title=document.title.replace(a,"*bleep*"));for(let c=0,d;d=b.snapshotItem(c++);)-1!==d.data.search(a)&&(d.data=d.data.replace(a,"*bleep*"));
 //let et = performance.now();
 //console.log("Milliseconds to complete: " + (et - st).toString());
