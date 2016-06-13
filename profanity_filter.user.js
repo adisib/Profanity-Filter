a7b7ea294ddb33293bc0e30f03e4a1568842fa40
Alex Disibio <alexdisibio@gmail.com>
2016-06-13 14:24:45 -0400

    Shorten word list more

--- profanity_filter.user.js
+++ profanity_filter.user.js
@@ -3,7 +3,7 @@
 // @author        adisib
 // @namespace     namespace_adisib
 // @description   Basic filtering for profanity from website text. Designed to have minimal performance impact.
-// @version       2016.06.02
+// @version       2016.06.13
 // @include       http://*
 // @include       https://*
 // @noframes
@@ -32,11 +32,11 @@
 
     // words to be filtered list
     // This should be ordered by most common first for performance (still TODO, but not important)
-    const words = ['fuck','shit','ass','damn','asshole','bullshit','bitch','piss','sh!t','jackass','goddamn','crap','bastard','cunt','dumbass','fag','douche','shitt','shitfull','shiz','pissoff','nigg','nigga','motherfuck','mothafucka','mothafuck','fuk','fukk','fuckme','fcuk','b!tch','phuq','phukk','phuk','phuck','feg','fatass','faggot','fagot','faggit','fagg','fagget','dipshit','assfuck','buttfuck','asswipe','asskiss','assclown'];
+    const words = ['fuck','shit','ass','damn','asshole','bullshit','bitch','piss','sh!t','jackass','goddamn','crap','bastard','cunt','dumbass','fag','douche','shitfull','shiz','pissoff','nig','nigga','motherfuck','mothafucka','mothafuck','fuk','fuckme','fcuk','b!tch','phuq','phuk','phuck','feg','fatass','faggot','fagot','faggit','fagget','dipshit','assfuck','buttfuck','asswipe','asskiss','assclown'];
 
     // filters the words and any versions with optional endings
     // shouldn't run into issues with optional endings; a whitelist would be trivial to implement should it be required
-    const wordsFilter = new RegExp("\\b(?:" + words.join("|") + ")(?:in(?:g)?|ed|(?:i)??er)??(?:s|es|y|z)??\\b", "gi");
+    const wordsFilter = new RegExp("\\b(?:" + words.join("|") + ")[tgk]??(?:in(?:g)?|ed|(?:i)??er)??(?:s|es|y|z)??\\b", "gi");
 
 
 
