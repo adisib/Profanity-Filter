b9e61f294bd76c549d0c5de76b1468441d4d9d7e
adisib <alexdisibio@gmail.com>
2016-08-06 17:01:12 -0400

    Minor word list adjustments

--- profanity_filter_compiled.user.js
+++ profanity_filter_compiled.user.js
@@ -20,7 +20,7 @@
 (function(){
 "use strict";
 //let st = performance.now();
-let a=new RegExp("\\b(?:(?:f(?:a(?:g(?:(?:g[eio]|o)t)??|tass)|u(?:ck(?:me)??|k)|(?:cu|vc)k|eg)|b(?:u(?:llshit|ttfuck)|[!i]tch|astard)|ass(?:(?:hol|wip)e|clown|fuck|kiss)??|d(?:amn|ipshit|umbass|ouche)|p(?:hu(?:c?k|q)|iss(?:off)??)|sh(?:i(?:t(?:full)??|z)|!t)|moth(?:afucka?|erfuck)|c(?:rap|unt)|nig(?:ga)?|goddamn|jackass))[tgk]??(?=(?:ing?|ed|i??er)??(?:[syz]|ess??)??\\b)","gi"),b=document.evaluate("./*[not(self::script or self::noscript or self::code)]//text()[string-length(normalize-space()) > 2]",
+let a=new RegExp("\\b(?:(?:f(?:a(?:g(?:(?:g[eio]|o)t)??|tass)|u(?:ck(?:me)??|k)|(?:cu|vc)k|eg)|b(?:u(?:llshit|ttfuck)|[!i]tch|astard)|ass(?:(?:hol|wip)e|fuck|kiss|clown)??|d(?:amn|umbass|ouche(?:bag)??|ipshit)|p(?:hu(?:c?k|q)|iss(?:off)??)|sh(?:i(?:t(?:full)??|z)|!t)|moth(?:er|a)fuck|c(?:rap|unt)|goddamn|jackass|nig))[tgk]??(?=(?:(?:ing?|ed|i??er|a))??(?:[syz]|ess??)??\\b)","gi"),b=document.evaluate("./*[not(self::script or self::noscript or self::code)]//text()[string-length(normalize-space()) > 2]",
 document.body,null,XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE,null);if(a.test(document.title))document.title=document.title.replace(a,"*bleep*");for(let c=b.snapshotLength,d=0;d<c;++d){let e=b.snapshotItem(d);if(a.test(e.data))e.data=e.data.replace(a,"*bleep*");}
 //let et = performance.now();
 //console.log("PF | Run-Time (ms): " + (et - st).toString());
