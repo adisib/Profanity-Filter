6804e828d2b30a689369388b01462965492cc557
adisib <alexdisibio@gmail.com>
2016-08-15 19:13:58 -0400

    Fix -ess false positive

--- profanity_filter.user.js
+++ profanity_filter.user.js
@@ -36,7 +36,7 @@
 
     // filters the words and any versions with optional endings
     // shouldn't run into issues with optional endings; a whitelist would be trivial to implement should it be required
-    const wordsFilter = new RegExp("\\b(?:" + words.join("|") + ")[tgk]??(?=(?:ing?|ed|i??er|a)??(?:[syz]|ess??)??\\b)", "gi");
+    const wordsFilter = new RegExp("\\b(?:" + words.join("|") + ")[tgk]??(?=(?:ing?(:?ess)??|ed|i??er|a)??(?:[syz]|es)??\\b)", "gi");
 
 
 
